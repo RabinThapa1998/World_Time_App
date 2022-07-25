5effa9df3ce1e736e18872b8f4124f2edba6b733
RabinThapa1998 <imrabin1998@gamil.com>
2022-07-25 23:37:14 +0545

    feat: add world time api

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,18 +8,19 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+ 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[900],
-        title:Text("Choose Location"),  
+        title: Text("Choose Location"),
         centerTitle: true,
         elevation: 0,
       ),
       body: Text('Choose Location'),
-    
     );
   }
 }
