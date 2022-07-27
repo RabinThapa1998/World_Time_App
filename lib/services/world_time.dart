0433afeb25938d09311dd52fd1d8d4cfb0c51db2
RabinThapa1998 <imrabin1998@gamil.com>
2022-07-27 09:46:23 +0545

    feat: add world time api and slash screen loading

---
+++ lib/services/world_time.dart
@@ -0,0 +1,33 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  late String location;
+  late String time;
+  late String flag;
+  late String url;
+
+  WorldTime({required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    try {
+      var urlParsed = Uri.parse('https://worldtimeapi.org/api/timezone/$url');
+
+      //make the request
+      Response response = await get(urlParsed);
+      Map data = jsonDecode(response.body);
+      // print(data);
+      //get properties from data
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1, 3);
+
+      //create datatime obj
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      time = now.toString();
+    } catch (e) {
+      print('Caught an error : $e');
+      time = 'could not get time data';
+    }
+  }
+}
